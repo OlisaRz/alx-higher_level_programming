1bfa9207b609026bdd939e079b1cd7df0c357b01
Rosemary Okonda <olisaokonda@gmail.com>
2023-09-06 05:33:06 +0000

    program will print ASCII alphabet except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) not in 'eq'
-    print('{}' .format(chr(i)), end='')
+    if (chr(i) != 'q' and chr(i) != 'e'):
+        print('{0:s}' .format(chr(i)), end='')
